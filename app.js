1ac6648671bc951479304a00fe68a83d9d7f9752
Desiree Mendes <desireefrost@live.ca>
2022-02-06 23:47:35 -0500

    Change port for heroku

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
 app.use(express.static("public"));
 
-app.listen(process.env.PORT || PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`App listening on ${PORT}...`);
 });
